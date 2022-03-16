ce40b144deed91d3a2b86e25378faa03c825b1e9
YuricsaMay <75384236+YuricsaMay@users.noreply.github.com>
2022-03-16 16:36:59 -0500

    informacion actividad

---
+++ app/src/main/java/com/example/fruitsandvegetables/Ciruela.kt
@@ -0,0 +1,15 @@
+package com.example.fruitsandvegetables
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+class Anona : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anona)
+
+
+    }
+}
